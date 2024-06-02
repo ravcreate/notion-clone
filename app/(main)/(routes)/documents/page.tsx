84c12abcf3f5ca6d5b13f7bac29b2ac58f07b825
ravcreate <ravuthvann@outlook.com>
2024-06-01 19:51:24 -0700

    added all items

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,4 +1,21 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { useUser } from "@clerk/clerk-react"
+import { PlusCircle } from "lucide-react"
+
 const DocumentsPage = () => {
-    return <div>This is a protected DocumentsPage</div>
+    const { user } = useUser()
+
+    return (
+        <div className="h-full flex flex-col items-center justify-center space-y-4">
+            <h2 className="text-lg font-medium">
+                Welcome to {user?.firstName}&apos; Flowtion
+            </h2>
+            <Button>
+                <PlusCircle className="h-4 w-4 mr-2" /> Create a note
+            </Button>
+        </div>
+    )
 }
 export default DocumentsPage
